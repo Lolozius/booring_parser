767b310ca8c1f3cbbb4c57822481383ce096b20e
Lolozius <61847373+Lolozius@users.noreply.github.com>
2023-07-03 19:20:47 +0300

    start eaeaedae

--- parser/parser.py
+++ parser/parser.py
@@ -1,19 +1,38 @@
+import time
+
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(cache_valid_range=10).install()))
-link = input()
-url = link
-driver.get(url)
-time.sleep(5)
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-timer = soup.body
-swag = timer.table('div', id='nexttime')
-for t in swag:
-    h = t.getText()
-    pass
-    print(h)
+
+class BoringParser:
+    """
+    Пока нет идей куда применить в будущем не забыть чё нить своять
+    (Внешнее приложение)
+    """
+    def __init__(self, link):
+        self.link = link
+
+    def parser(self):
+        """
+        Парсер подцепляет таймер с карточки странницы animevost
+        """
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(cache_valid_range=10).install()))
+        driver.get(self.link)
+        try:
+            requests.head('https://animevost.org/')
+            time.sleep(1)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            timer = soup.body.table('div', id='nexttime')
+            for t in timer:
+                h = t.getText()
+                print(h)
+        except requests.ConnectionError:
+            print('Я днс не чувствую..')
+
+
+time_vallue = BoringParser(input())
+time_vallue.parser()
--- parser/test_script.py
+++ parser/test_script.py
@@ -4,10 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from requests import Session
 from bs4 import BeautifulSoup
+import mechanicalsoup
 
-
-url = 'https://animevost.org/tip/tv/2959-kaminaki-sekai-no-kamisama-katsudou.html'
-ses = Session()
-response = ses.get(url)
-with open('test.html', 'w') as r:
-    r.write(response.text)
+browser = mechanicalsoup.StatefulBrowser()
+browser.open('https://animevost.org/tip/tv/2959-kaminaki-sekai-no-kamisama-katsudou.html')
